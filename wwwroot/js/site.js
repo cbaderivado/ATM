568df16eb3c415b35ede82ac18e5644b0190cab9
cbaderivado <cbaderivado54@gmail.com>
2023-01-31 19:06:06 -0300

    actualizado bootstrap e iniciado con el home

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,22 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+function validarNroTarjeta() {
+
+	
+	var numero = document.getElementById('numeroTarjeta');
+
+	
+	var valido = "^[0-9]{16,16}$";
+
+	// Using test we can check if the text match the pattern
+	if (valido.test(numero.value)) {
+		alert('OK');
+		return true;
+	} else {
+		alert('error en el numero de tarjeta');
+		return false;
+	}
+} 
